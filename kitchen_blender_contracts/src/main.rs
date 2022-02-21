e1a40992973d832ec9538286b579246e98af82e5
beris147 <edwir147@gmail.com>
2022-02-20 19:43:39 -0800

    contracts better defined

--- kitchen_blender_contracts/src/main.rs
+++ kitchen_blender_contracts/src/main.rs
@@ -1,4 +1,5 @@
 use contracts;
+use contracts::invariant;
 
 trait FoodProcessor {
     fn get_speed(&self) -> i32;
@@ -12,12 +13,15 @@ struct Blender {
     speed: i32,
     full: bool,
 }
-// #[contracts::invariant()]
+
+#[contracts::invariant(self.speed>=0)]
+#[contracts::invariant(self.speed<10)]
 impl FoodProcessor for Blender {
     fn get_speed(&self) -> i32 {
         return self.speed;
     }
-    #[contracts::requires(i32::abs(self.get_speed() - speed) == 1 && (self.is_full() || speed == 0))]
+    #[contracts::requires(i32::abs(self.get_speed() - speed) == 1 )]
+    #[contracts::requires(self.is_full() || speed == 0)]
     #[contracts::ensures(self.get_speed() == speed)]
     fn set_speed(&mut self, speed: i32) {
         self.speed = speed;
@@ -25,13 +29,13 @@ impl FoodProcessor for Blender {
     fn is_full(&self) -> bool {
         return self.full;
     }
-    #[contracts::requires(self.is_full() == false)]
-    #[contracts::ensures(self.is_full() == true)]
+    #[contracts::requires(!self.is_full())]
+    #[contracts::ensures(self.is_full())]
     fn fill(&mut self) {
         self.full = true;
     }
-    #[contracts::requires(self.is_full() == true)]
-    #[contracts::ensures(self.is_full() == false)]
+    #[contracts::requires(self.is_full())]
+    #[contracts::ensures(!self.is_full())]
     fn empty(&mut self) {
         self.full = false;
     }
